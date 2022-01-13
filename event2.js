9f2d29c15d5cdae257e39b0319c21364353ff289
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-13 13:26:19 +0900

    modal

---
+++ event2.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+
+  $('.close-modal').click(function(){
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+})
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ①jquerryの公式ページから最新のjquerry読み込みコードをコピーし、
 htmlファイルに貼り付ける。
+( <script>はCSSファイルの読み込みのように<head>タグの中にも書けるが、</body>の直前に書くことで、WEBページの表示速度をより早めることが出来る。)
 (参考 https://style.potepan.com/articles/19871.html#HTMLjQuery)
