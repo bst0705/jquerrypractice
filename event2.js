085367e49badd4fd0fab3e9240a86a8c06174090
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-15 18:24:16 +0900

    add accordion

--- event2.js
+++ event2.js
@@ -57,9 +57,13 @@ $(function(){
     let $answer = $(this).find('.answer');
     if ($answer.hasClass('open')){
       $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
     }
     else{
       $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
     }
     })
 });
