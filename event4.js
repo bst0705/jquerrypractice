eee7541381090459258a93a6860a44c4130bb3d5
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-17 16:45:50 +0900

    get html, attr

---
+++ event4.js
@@ -0,0 +1,22 @@
+/*
+htmlメソッド, cssメソッド
+  jQueryで値をセットするメソッドは大抵ゲットにも使うことができます。
+  htmlメソッドとcssメソッドも、ゲットとして使うことができる。
+
+HTMLの属性
+  HTMLのタグにはclassやid、srcといったものを指定でき、これらを属性と呼びます。
+
+  HTMLの属性はattrメソッドを用いて「ゲット」と「セット」ができます。例えば、attr('id', 'title')のように第一引数に属性名、第二引数にその属性値を指定することで属性をセットすることができます。そして、第二引数を指定しない場合は、その属性の値を取得できます。*/
+
+$(function() {
+  var title = $('#title').text();
+  // 2つの変数を定義してください
+  var id = $('.section-content').find('p').attr('id');
+  var href = $('#link').attr('href');
+
+  $('#title-text').text(title);
+  // textメソッドを用いて、それぞれ表示してください
+  $('#content-id').text(id);
+  $('#link-href').text(href);
+  
+});
