0eed876a5aeaaab609f593a2353d1d851acdbdae
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-17 16:59:54 +0900

    change code

--- event4.js
+++ event4.js
@@ -10,13 +10,10 @@ HTMLの属性
 
 $(function() {
   var title = $('#title').text();
-  // 2つの変数を定義してください
   var id = $('.section-content').find('p').attr('id');
   var href = $('#link').attr('href');
 
   $('#title-text').text(title);
-  // textメソッドを用いて、それぞれ表示してください
   $('#content-id').text(id);
   $('#link-href').text(href);
-  
 });
